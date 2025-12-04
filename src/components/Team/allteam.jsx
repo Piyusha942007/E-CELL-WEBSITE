79d639b069af7de477d399e37257fdc5efd4034c
Piyusha942007 <piyushaamrutkar41@gmail.com>
2025-12-04 16:59:10 +0530

    Changed the team hierarchy

--- src/components/Team/allteam.jsx
+++ src/components/Team/allteam.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useNavigate } from "react-router-dom"
 import "./team.css"
 
@@ -35,163 +33,169 @@ const TeamCard = ({ member, linkedin }) => {
 const AllTeam = () => {
   const navigate = useNavigate()
 
-  const teamData = {
-    coreTeam: [
-      {
-        name: "Aditya Divate",
-        position: "Head of Operations",
-        image: "/E-CELL-WEBSITE/images/team/adityaa.jpeg",
-        linkedin: "https://www.linkedin.com/in/aditya-divate-4b7b5726b/",
-      },
-      {
-        name: "Advait Kulkarni",
-        position: "Chair, ECELL",
-        image: "/E-CELL-WEBSITE/images/team/Advait.jpg",
-        linkedin: "https://www.linkedin.com/in/advait-kulkarni-k168/",
-      },
-      {
-        name: "Kadambari Khedekar",
-        position: "Deputy Chair",
-        image: "/E-CELL-WEBSITE/images/team/.jpg",
-        linkedin: "https://www.linkedin.com/in/kadambari-arvind-khedekar-203100327/",
-      },
-      {
-        name: "Siddhesh Kirve",
-        position: "Secretary",
-        image: "/E-CELL-WEBSITE/images/team/Siddhesh.jpg",
-        linkedin: "https://www.linkedin.com/in/siddhesh-kirve-2aa2ba329/",
-      },
-      {
-        name: "Krish Sarode",
-        position: "Head of Finance",
-        image: "/E-CELL-WEBSITE/images/team/Krish.jpg",
-        linkedin: "https://www.linkedin.com/in/krish-sarode-68b53332b/",
-      },
-      {
-        name: "Disha Marne",
-        position: "Joint Head of Finance and Resources",
-        image: "/E-CELL-WEBSITE/images/team/.jpg",
-        linkedin: "https://www.linkedin.com/in/disha-marne-64a74a292/",
-      },
-    ],
-    executiveTeam: [
-      {
-        name: "Himanshee Jaiswal",
-        position: "Chair, External Affairs",
-        image: "/E-CELL-WEBSITE/images/team/Himanshee.jpg",
-        linkedin: "https://www.linkedin.com/in/himansheejaiswal/",
-      },
-      {
-        name: "Akash Patil",
-        position: "Joint Head of Execution and Personal Relations",
-        image: "/E-CELL-WEBSITE/images/team/akashh.jpeg",
-        linkedin: "https://www.linkedin.com/in/akash-patil-b1119a32a/",
-      },
-      {
-        name: "Ketaki Shivange",
-        position: "Member",
-        image: "/E-CELL-WEBSITE/images/team/Ketaki.jpg",
-        linkedin: "https://www.linkedin.com/in/ketaki-shivange-18005932b/",
-      },
-      {
-        name: "Ritika Kolte",
-        position: "Member",
-        image: "/E-CELL-WEBSITE/images/team/Ritika.jpg",
-        linkedin: "https://www.linkedin.com/in/kolte-ritika-889832312/",
-      },
-    ],
-    technicalTeam: [
-      {
-        name: "Piyusha Amrutkar",
-        position: "Head of Technical Operations",
-        image: "/E-CELL-WEBSITE/images/team/Piyusha.jpg",
-        linkedin: "https://www.linkedin.com/in/piyusha-amrutkar-901918328/",
-      },
-      {
-        name: "Samiksha Ner",
-        position: "Joint Head of Technical Operations",
-        image: "/E-CELL-WEBSITE/images/team/Samiksha.png",
-        linkedin: "https://www.linkedin.com/in/samikshaner/",
-      },
-    ],
-    socialMediaTeam: [
-      {
-        name: "Anushka Agam",
-        position: "Head and content strategist for online presence,branding and social media",
-        image: "/E-CELL-WEBSITE/images/team/ANUSHKA.jpg",
-        linkedin: "https://www.linkedin.com/in/anushka-agam-1b552632b/",
-      },
-      {
-        name: "Aryan Digambare",
-        position: "Joint Head for online presence,branding and social media",
-        image: "/E-CELL-WEBSITE/images/team/Aryan.jpg",
-        linkedin: "https://www.linkedin.com/in/aryan-digambare-76105732b/",
-      },
-      {
-        name: "Mrunmayee Khot",
-        position: "Member",
-        image: "/E-CELL-WEBSITE/images/team/Mrunmayee.jpg",
-        linkedin: "https://www.linkedin.com/in/mrunmayee-khot-14a06132b/",
-      },
-    ],
-    marketingTeam: [
-      {
-        name: "Esha Pansare",
-        position: "Head of Marketing",
-        image: "/E-CELL-WEBSITE/images/team/Esha.png",
-        linkedin: "https://www.linkedin.com/in/esha-pansare-6106ab290/",
-      },
-      {
-        name: "Shreyas Kadav",
-        position: "Chair, Internal Affairs",
-        image: "/E-CELL-WEBSITE/images/team/Shreyas.png",
-        linkedin: "https://www.linkedin.com/in/shreyas-kadav-67b213323/",
-      },
-      {
-        name: "Aditya Kute",
-        position: "Member",
-        image: "/E-CELL-WEBSITE/images/team/.jpg",
-        linkedin: "https://www.linkedin.com/in/aditya-kute-732bb8328/",
-      },
-      {
-        name: "Shridhar Kulkarni",
-        position: "Member",
-        image: "/E-CELL-WEBSITE/images/team/Shridhar.jpg",
-        linkedin: "https://www.linkedin.com/in/shridhar-kulkarni-374b93200/",
-      },
-    ],
-    designTeam: [
-      {
-        name: "Utkarsh Kashid",
-        position: "Head of Design and Visual Communications",
-        image: "/placeholder.svg",
-        linkedin: "https://www.linkedin.com/in/utkarsh-kashid-44843132b/",
-      },
-      {
-        name: "Aditya Kashid",
-        position: " Joint Head of Design and Visual Communications ",
-        image: "/E-CELL-WEBSITE/images/team/Adityaka.jpg",
-        linkedin: "https://www.linkedin.com/in/adityakashid746/",
-      },
-      {
-        name: "Manthan Patil",
-        position: "Member",
-        image: "/E-CELL-WEBSITE/images/team/Manthan.jpg",
-        linkedin: "https://www.linkedin.com/in/manthan-patil-738812377/",
-      },
-    ],
-  }
 
-  const TeamSection = ({ title, members }) => (
-    <section className="team-section">
-      <h2 className="section-title">{title}</h2>
-      <div className="team-grid">
-        {members.map((member, index) => (
-          <TeamCard key={index} member={member} linkedin={member.linkedin} />
-        ))}
-      </div>
-    </section>
-  )
+  // 2. High Command (Ops, Chair, Deputy)
+  const highCommand = [
+    {
+      name: "Aditya Divate",
+      position: "Head of Operations",
+      image: "/E-CELL-WEBSITE/images/team/adityaa.jpeg",
+      linkedin: "https://www.linkedin.com/in/aditya-divate-4b7b5726b/",
+    },
+    {
+      name: "Advait Kulkarni",
+      position: "Chair, ECELL",
+      image: "/E-CELL-WEBSITE/images/team/Advait.jpg",
+      linkedin: "https://www.linkedin.com/in/advait-kulkarni-k168/",
+    },
+    {
+      name: "Kadambari Khedekar",
+      position: "Deputy Chair",
+      image: "/E-CELL-WEBSITE/images/team/.jpg", 
+      linkedin: "https://www.linkedin.com/in/kadambari-arvind-khedekar-203100327/",
+    },
+  ]
+
+  // 3. Admin Core
+  const adminCore = [
+    {
+      name: "Himanshee Jaiswal",
+      position: "Chair, External Affairs",
+      image: "/E-CELL-WEBSITE/images/team/Himanshee.jpg",
+      linkedin: "https://www.linkedin.com/in/himansheejaiswal/",
+    },
+    {
+      name: "Shreyas Kadav",
+      position: "Chair, Internal Affairs",
+      image: "/E-CELL-WEBSITE/images/team/Shreyas.png",
+      linkedin: "https://www.linkedin.com/in/shreyas-kadav-67b213323/",
+    },
+    {
+      name: "Siddhesh Kirve",
+      position: "Secretary",
+      image: "/E-CELL-WEBSITE/images/team/Siddhesh.jpg",
+      linkedin: "https://www.linkedin.com/in/siddhesh-kirve-2aa2ba329/",
+    },
+  ]
+
+  // 4. Finance (Separated for Mobile Styling)
+  const financeTeam = [
+    {
+      name: "Krish Sarode",
+      position: "Head of Finance",
+      image: "/E-CELL-WEBSITE/images/team/Krish.jpg",
+      linkedin: "https://www.linkedin.com/in/krish-sarode-68b53332b/",
+    },
+    {
+      name: "Disha Marne",
+      position: "Joint Head of Finance and Resources",
+      image: "/E-CELL-WEBSITE/images/team/.jpg", 
+      linkedin: "https://www.linkedin.com/in/disha-marne-64a74a292/",
+    },
+  ]
+
+  // 5. Execution Team (Akash, Ketaki, Ritika)
+  const executionTeam = [
+    {
+      name: "Akash Patil",
+      position: "Joint Head of Execution and PR",
+      image: "/E-CELL-WEBSITE/images/team/akashh.jpeg",
+      linkedin: "https://www.linkedin.com/in/akash-patil-b1119a32a/",
+    },
+    {
+      name: "Ketaki Shivange",
+      position: "Member",
+      image: "/E-CELL-WEBSITE/images/team/Ketaki.jpg",
+      linkedin: "https://www.linkedin.com/in/ketaki-shivange-18005932b/",
+    },
+    {
+      name: "Ritika Kolte",
+      position: "Member",
+      image: "/E-CELL-WEBSITE/images/team/Ritika.jpg",
+      linkedin: "https://www.linkedin.com/in/kolte-ritika-889832312/",
+    },
+  ]
+
+  // 6. Technical Operations (Piyusha, Samiksha)
+  const techTeam = [
+    {
+      name: "Piyusha Amrutkar",
+      position: "Head of Technical Operations",
+      image: "/E-CELL-WEBSITE/images/team/Piyusha.jpg",
+      linkedin: "https://www.linkedin.com/in/piyusha-amrutkar-901918328/",
+    },
+    {
+      name: "Samiksha Ner",
+      position: "Joint Head of Technical Operations",
+      image: "/E-CELL-WEBSITE/images/team/Samiksha.png",
+      linkedin: "https://www.linkedin.com/in/samikshaner/",
+    },
+  ]
+
+  // 7. Social Media
+  const socialMediaTeam = [
+    {
+      name: "Anushka Agam",
+      position: "Head - Social Media & Branding",
+      image: "/E-CELL-WEBSITE/images/team/ANUSHKA.jpg",
+      linkedin: "https://www.linkedin.com/in/anushka-agam-1b552632b/",
+    },
+    {
+      name: "Aryan Digambare",
+      position: "Joint Head - Social Media",
+      image: "/E-CELL-WEBSITE/images/team/Aryan.jpg",
+      linkedin: "https://www.linkedin.com/in/aryan-digambare-76105732b/",
+    },
+    {
+      name: "Mrunmayee Khot",
+      position: "Member",
+      image: "/E-CELL-WEBSITE/images/team/Mrunmayee.jpg",
+      linkedin: "https://www.linkedin.com/in/mrunmayee-khot-14a06132b/",
+    },
+  ]
+
+  const designTeam = [
+    {
+      name: "Utkarsh Kashid",
+      position: "Head of Design",
+      image: "/placeholder.svg",
+      linkedin: "https://www.linkedin.com/in/utkarsh-kashid-44843132b/",
+    },
+    {
+      name: "Aditya Kashid",
+      position: "Joint Head of Design",
+      image: "/E-CELL-WEBSITE/images/team/Adityaka.jpg",
+      linkedin: "https://www.linkedin.com/in/adityakashid746/",
+    },
+    {
+      name: "Manthan Patil",
+      position: "Member",
+      image: "/E-CELL-WEBSITE/images/team/Manthan.jpg",
+      linkedin: "https://www.linkedin.com/in/manthan-patil-738812377/",
+    },
+  ]
+  // 8. Marketing
+  const marketingTeam = [
+    {
+      name: "Esha Pansare",
+      position: "Head of Marketing",
+      image: "/E-CELL-WEBSITE/images/team/Esha.png",
+      linkedin: "https://www.linkedin.com/in/esha-pansare-6106ab290/",
+    },
+    {
+      name: "Aditya Kute",
+      position: "Member",
+      image: "/E-CELL-WEBSITE/images/team/.jpg", 
+      linkedin: "https://www.linkedin.com/in/aditya-kute-732bb8328/",
+    },
+    {
+      name: "Shridhar Kulkarni",
+      position: "Member",
+      image: "/E-CELL-WEBSITE/images/team/Shridhar.jpg",
+      linkedin: "https://www.linkedin.com/in/shridhar-kulkarni-374b93200/",
+    },
+  ]
+
 
   return (
     <div className="team-container">
@@ -200,12 +204,67 @@ const AllTeam = () => {
         <h1 className="team-header-title">E-CELL Team 2025</h1>
       </div>
 
-      <TeamSection title="Core Team" members={teamData.coreTeam} />
-      <TeamSection title="Executive Team" members={teamData.executiveTeam} />
-      <TeamSection title="Technical Team" members={teamData.technicalTeam} />
-      <TeamSection title="Social Media Team" members={teamData.socialMediaTeam} />
-      <TeamSection title="Marketing Team" members={teamData.marketingTeam} />
-      <TeamSection title="Design Team" members={teamData.designTeam} />
+      <section className="team-section">
+        <div className="hierarchy-section">
+          
+
+          {/* 2. Ops, Chair, Deputy */}
+          <div className="hierarchy-row">
+            {highCommand.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 3. Ext, Int, Sec */}
+          <div className="hierarchy-row">
+            {adminCore.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 4. Finance (FORCED SIDE-BY-SIDE MOBILE) */}
+          <div className="finance-row">
+            {financeTeam.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 5. Execution (Moved up) */}
+          <div className="hierarchy-row">
+            {executionTeam.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 6. Technical */}
+          <div className="hierarchy-row">
+            {techTeam.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 7. Social Media */}
+          <div className="hierarchy-row">
+            {socialMediaTeam.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 8. Marketing */}
+          <div className="hierarchy-row">
+            {designTeam.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+
+          {/* 9. Design */}
+          <div className="hierarchy-row">
+            {marketingTeam.map((member, index) => (
+              <TeamCard key={index} member={member} linkedin={member.linkedin} />
+            ))}
+          </div>
+        </div>
+      </section>
 
       <div className="back-button-container">
         <button onClick={() => navigate("/team")} className="back-button">
