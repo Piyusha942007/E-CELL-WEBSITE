c04a00bc6b2cae7c0e9dbf1a3f0a4b708f86eed7
Piyusha942007 <piyushaamrutkar41@gmail.com>
2025-12-05 15:49:49 +0530

    Faculty advsior photo

--- src/components/Team/team.jsx
+++ src/components/Team/team.jsx
@@ -42,7 +42,7 @@ const Team = () => {
     {
       name: "Dr.Vaishali Kulloli",
       position: "Faculty Advisor",
-      image: "/E-CELL-WEBSITE/images/team/vaishali.jpg",
+      image: "/E-CELL-WEBSITE/images/team/vaishali.png",
       linkedin: "https://www.linkedin.com/in/dr-vaishali-kulloli-3b4685326/",
     },
     {
